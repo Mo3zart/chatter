9e12dc4f79bac3bb4d9879ec6e79838218544825
Mo3zart <moritz.tech@proton.me>
2024-05-07 12:16:33 +0200

    Initial Commit

---
+++ chatter/chatterApp.swift
@@ -0,0 +1,17 @@
+//
+//  chatterApp.swift
+//  chatter
+//
+//  Created by moritz.zewinger on 07.05.24.
+//
+
+import SwiftUI
+
+@main
+struct chatterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
